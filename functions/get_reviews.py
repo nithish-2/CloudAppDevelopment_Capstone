e4811358ec40248fa3c793272faf97ef855abe2c
nithish-2 <69504251+nithish-2@users.noreply.github.com>
2022-08-30 12:18:03 +0530

    Update get_reviews.py

--- functions/get_reviews.py
+++ functions/get_reviews.py
@@ -3,9 +3,9 @@ from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def main(dict): 
-    authenticator = IAMAuthenticator("w3JLN-xcIwp1na8L_LhGbP9zcucQnw_xCsjny-alZDx8")
+    authenticator = IAMAuthenticator("jBPu0G2aHNY0RATNq_c9r693XE4qM2ZF4SkbkfSDJHao")
     service = CloudantV1(authenticator=authenticator)
-    service.set_service_url("https://991a67c6-1de1-4f7b-8d62-854751829d03-bluemix.cloudantnosqldb.appdomain.cloud")
+    service.set_service_url("https://24b3737b-fec5-4b41-bd68-8f6b28b7429d-bluemix.cloudantnosqldb.appdomain.cloud")
     response = service.post_find(
                 db='reviews',
                 selector={'dealership': {'$eq': int(dict['id'])}},
