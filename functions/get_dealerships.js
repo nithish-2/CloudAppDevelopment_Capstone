213eed610184e306ff3796b9448c1c29a41a3c8a
nithish-2 <69504251+nithish-2@users.noreply.github.com>
2022-08-30 12:17:26 +0530

    Update get_dealerships.js

--- functions/get_dealerships.js
+++ functions/get_dealerships.js
@@ -3,11 +3,11 @@ function main(params) {
     return new Promise(function (resolve, reject) {
         const { CloudantV1 } = require('@ibm-cloud/cloudant');
         const { IamAuthenticator } = require('ibm-cloud-sdk-core');
-        const authenticator = new IamAuthenticator({ apikey: 'w3JLN-xcIwp1na8L_LhGbP9zcucQnw_xCsjny-alZDx8' })
+        const authenticator = new IamAuthenticator({ apikey: 'jBPu0G2aHNY0RATNq_c9r693XE4qM2ZF4SkbkfSDJHao' })
         const cloudant = CloudantV1.newInstance({
             authenticator: authenticator
         });
-        cloudant.setServiceUrl('https://991a67c6-1de1-4f7b-8d62-854751829d03-bluemix.cloudantnosqldb.appdomain.cloud');
+        cloudant.setServiceUrl('https://24b3737b-fec5-4b41-bd68-8f6b28b7429d-bluemix.cloudantnosqldb.appdomain.cloud');
         if (params.st) {
             // return dealership with this state 
             cloudant.postFind({db:'dealerships',selector:{st:params.st}})
